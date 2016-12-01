87621b350db817fdc17da792fd5cbf7366ce615d
ricklen <ricklenderink93@gmail.com>
2016-12-01 16:02:41 +0100

    Formatting example

---
+++ WeekThree/src/formatter/Formatter.java
@@ -0,0 +1,14 @@
+package formatter;
+
+public class Formatter {
+	
+	public static void printLine(String text, double price){
+		System.out.printf("%-10.10s %10.2f", text, price);
+	}
+
+	public static void main (String[] args){
+		printLine("bla", 2.10);
+		
+	}
+
+}
